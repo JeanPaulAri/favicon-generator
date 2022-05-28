4306b04835d7e760064373e7e5ef8b0dbc07f50e
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-05-28 01:01:01 -0500

    adding function return value (html div container)

--- src/Components/Pixel-editor.jsx
+++ src/Components/Pixel-editor.jsx
@@ -1,77 +1,24 @@
-import React, {useState} from "react";
+import React from "react";
 import "../Styles/Pixel-editor.scss"
-import {CirclePicker} from "react-color";
-import DrawingPanel from "./DrawingPanel";
 
 
 export default function PixelEditor(){
-    const [panelWidth, setPanelWidth] = useState(16);
-    const [panelHeight, setPanelHeight] = useState(16);
-    const [hideOptions, setHideOptions] = useState(false);
-    const [hideDrawingPanel, setHideDrawingPanel] = useState(true);
-    const [buttonText, setButtonText] = useState("start drawing");
-    const [selectedColor, setColor] = useState("#f44336");
 
-    function initializeDrawingPanel() {
-        setHideOptions(!hideOptions);
-        setHideDrawingPanel(!hideDrawingPanel);
+    return(
 
-        buttonText === "start drawing"
-            ? setButtonText("new draw")
-            : setButtonText("start drawing");
-    }
+      <div id="PixelEditor">
+          <h1>Favicon Editor</h1>
+          <h3>ver 1.0</h3>
 
-    function changeColor(color) {
-        setColor(color.hex);
-    }
 
-    return (
-        <div id="editor">
-            <h1>Favicon Editor</h1>
-            {hideDrawingPanel && <h2>Enter Panel Dimensions</h2>}
-            {hideDrawingPanel && (
-                <div id="options">
-                    <div className="option">
-                        <input
-                            type="number"
-                            className="panelInput"
-                            defaultValue={panelWidth}
-                            onChange={(e) => {
-                                setPanelWidth(e.target.value);
-                            }}
-                        />
-                        <span>Width</span>
-                    </div>
-                    <div className="option">
-                        <input
-                            type="number"
-                            className="panelInput"
-                            defaultValue={panelHeight}
-                            onChange={(e) => {
-                                setPanelHeight(e.target.value);
-                            }}
-                        />
-                        <span>Height</span>
-                    </div>
-                </div>
-            )}
+          <div id="options">
+              <div className="option">
 
-            <button onClick={initializeDrawingPanel} className="button">
-                {buttonText}
-            </button>
+              </div>
+          </div>
+      </div>
 
-            {hideOptions && (
-                <CirclePicker color={selectedColor} onChangeComplete={changeColor} />
-            )}
-
-            {hideOptions && (
-                <DrawingPanel
-                    width={panelWidth}
-                    height={panelHeight}
-                    selectedColor={selectedColor}
-                />
-            )}
-        </div>
     );
+
 
 }
