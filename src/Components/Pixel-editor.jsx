c373803f7f6d39d769ebc9f5bdba67db3d4aafc7
Jean Paul Ari Maldonado <94414304+JeanPaulAri@users.noreply.github.com>
2022-05-28 14:49:51 -0500

    adding Styles

--- src/Components/Pixel-editor.jsx
+++ src/Components/Pixel-editor.jsx
@@ -1,21 +1,57 @@
-import React from "react";
+import React,{useState} from "react";
 import "../Styles/Pixel-editor.scss"
 
 
 export default function PixelEditor(){
+    const [panelWidth, setPanelWidth] = useState(16);
+    const [panelHeight, setPanelHeight] = useState(16);
+    const [hideOptions, setHideOptions] = useState(false);
+    const [hideDrawingPanel, setHideDrawingPanel] = useState(true);
+    const [buttonText, setButtonText] = useState("Render Canvas");
+    const [selectedColor, setColor] = useState("#000000");
 
+    function initializeDrawingPanel(){
+        setHideOptions(!hideOptions);
+        setHideDrawingPanel(!hideDrawingPanel);
+
+        buttonText === "Render Canvas"
+            ? setButtonText("Re-render")
+            : setButtonText("Render Canvas");
+    }
     return(
 
       <div id="PixelEditor">
           <h1>Favicon Editor</h1>
-          <h3>ver 1.0</h3>
+          <h2>ver 1.0</h2>
 
-
+          <h3>Canvas Size:</h3>
           <div id="options">
               <div className="option">
 
+                  <input
+                      type="number"
+                      className="panelInput"
+                      defaultValue={panelWidth}
+                      onChange={(e)=>
+                      {setPanelWidth(e.target.value)}}
+                  />
+                  <span>[ Width ]</span>
+
+              </div>
+              <div className="option">
+
+                  <input
+                      type="number"
+                      className="panelInput"
+                      defaultValue={panelHeight}
+                      onChange={(e)=>
+                      setPanelHeight(e.target.value)}
+                  />
+                  <span>[ Height ]</span>
+
               </div>
           </div>
+          <button className="button" onClick={initializeDrawingPanel}>{buttonText}</button>
       </div>
 
     );
